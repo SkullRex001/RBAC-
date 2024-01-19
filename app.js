747453984d5d05f28b55cffdce5e83d57d3e27cb
SkullRex001 <honestaditya@gmail.com>
2024-01-19 18:07:03 +0530

    did some ejs work

--- app.js
+++ app.js
@@ -4,24 +4,34 @@ const morgan = require('morgan')
 const mongoose = require('mongoose')
 require('dotenv').config()
 const app = express()
+app.use(express.json())
+app.use(express.static('public'))
 app.use(morgan('dev'))
+mongoose.connect(process.env.MONGO + 'RBAC2').then(()=>{
+    app.listen(port , (err)=>{
+        if(err) {
+            console.log(err) 
+    
+        }
+        else{
+            console.log(`port ${port} is running`)
+        }
+    })
+    
+
+}).catch((err)=>{
+    return console.log(err)
+    
+})
+
+app.set('view-engine' , 'ejs')
 
 const port = process.env.PORT || 3000
 
-app.listen(port , (err)=>{
-    if(err) {
-        console.log(err) 
 
-    }
-    else{
-        console.log(`port ${port} is running`)
-    }
-})
-
-app.get('/' , (req , res)=>{
-    res.send('hello')
-
-})
+app.use('/' , require('./routes/index'))
+app.use('/auth' , require('./routes/auth'))
+app.use('/user' , require('./routes/user'))
 
 app.use((req , res , next)=>{
     next(httpErrors.NotFound())
